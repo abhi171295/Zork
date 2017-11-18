15da50eb05d29e255a23fac302248f39848a042c
akanti <31495601+akanti@users.noreply.github.com>
2017-11-18 16:01:44 -0500

    Added code

--- Room.cpp
+++ Room.cpp
@@ -11,9 +11,28 @@ using namespace rapidxml;
 
 Room::Room(xml_node<>* roomName) {
 	// TODO Auto-generated constructor stub
-
+	createRoom(roomName);
 }
 
 Room::~Room() {
 	// TODO Auto-generated destructor stub
+}
+
+void Room::createRoom(xml_node<>* roomNode) {
+	string name;
+	string value;
+
+	roomNode = roomNode -> first_node();
+	while (roomNode != NULL) {
+		name = roomNode -> name();
+		value = roomNode -> value();
+
+		if (name == "name") {
+			this -> name = value;
+		}
+		if (name == "status") {
+			this -> name = value;
+		}
+
+	}
 }
